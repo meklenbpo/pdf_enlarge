f403b5acf2d14bac7403736579b03391ff7b9aa0
meklenbpo <yk@kantynent.com>
2021-01-12 11:59:01 +0300

    docstring update

--- pdf_enlarge.py
+++ pdf_enlarge.py
@@ -6,6 +6,6 @@ Scale the contents of a PDF document 2x.
 
 - Split each page into two parts along the perpendicular axis (e.g. one
   A4 page into two A5 pages).
-- Scale each split part 2 times
+- Scale each split part 2 times (e.g. A5 -> A4)
 - Merge scaled parts into a new PDF document.
 """
