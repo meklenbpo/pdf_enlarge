f5a85e8d7686494775b8b80b8404ee99aadc1312
meklenbpo <yk@kantynent.com>
2021-01-12 11:56:41 +0300

    Module docstring first draft

---
+++ pdf_enlarge.py
@@ -0,0 +1,11 @@
+"""
+PDF Enlarge
+===========
+
+Scale the contents of a PDF document 2x.
+
+- Split each page into two parts along the perpendicular axis (e.g. one
+  A4 page into two A5 pages).
+- Scale each split part 2 times
+- Merge scaled parts into a new PDF document.
+"""
