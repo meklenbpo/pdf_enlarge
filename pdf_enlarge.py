0cfe7f0ab95eedc86ce061915811779c845b21af
meklenbpo <yk@kantynent.com>
2021-01-13 12:28:35 +0300

    Added main() stub as an entry point for the console_scripts

--- pdf_enlarge.py
+++ pdf_enlarge.py
@@ -9,3 +9,15 @@ Scale the contents of a PDF document 2x.
 - Scale each split part 2 times (e.g. A5 -> A4)
 - Merge scaled parts into a new PDF document.
 """
+
+import sys
+
+
+def main() -> int:
+    """Console script entry point function."""
+    print('PDF Enlarge')
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main)
